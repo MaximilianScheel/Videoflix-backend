3df7bdba679a4621b36e6b5e800d53f8400af3b5
Maximilian Scheel <scheel.development@gmail.com>
2024-03-18 17:19:45 +0100

    login view

--- todolist/views.py
+++ todolist/views.py
@@ -7,7 +7,7 @@ from rest_framework.authtoken.models import Token
 class LoginView(ObtainAuthToken):
     def post(self, request, *args, **kwargs):
         serializer = self.serializer_class(data=request.data,
-                                        context={'request': request})
+                                           context={'request': request})
         serializer.is_valid(raise_exception=True)
         user = serializer.validated_data['user']
         token, created = Token.objects.get_or_create(user=user)
@@ -15,4 +15,4 @@ class LoginView(ObtainAuthToken):
             'token': token.key,
             'user_id': user.pk,
             'email': user.email
-        })
+        })  
--- videoflix/settings.py
+++ videoflix/settings.py
@@ -25,7 +25,14 @@ SECRET_KEY = 'django-insecure--qoxcnz%fztpgb!xubkiq@+^(2!6w0-x_un!y_$et@3jz8&7v8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1'
+]
+
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:4200"
+# ]
 
 
 # Application definition
@@ -38,10 +45,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'todolist',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
